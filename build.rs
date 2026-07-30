cc2761067adb9f43ee4a590c9dfa4fb67163e3d2
Stefan Einarsson <stefane81@gmail.com>
2026-07-30 00:52:03 +0000

    added daisyui

--- build.rs
+++ build.rs
@@ -1,7 +1,7 @@
 fn main() {
     // topcoat::tailwind::BuildConfig::new().render().unwrap();
     topcoat::tailwind::BuildConfig::new()
-        .input("styles.css")
+        .input("input.css")
         .render()
         .unwrap();
 }
